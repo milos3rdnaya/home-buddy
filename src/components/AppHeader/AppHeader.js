779ce8f7e3eed7c3cf21c453a0947826822e37db
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2022-11-25 19:06:57 +0400

    feat(header): header done

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,4 +1,6 @@
 import './appHeader.scss';
+import header from '../../assets/img/header.png';
+import nbc from '../../assets/img/nbc.png';
 
 export const AppHeader = () => {
 	return (
@@ -13,6 +15,19 @@ export const AppHeader = () => {
           </div>
           <div className='headerTop__anchor'>
             <a href='#'>Link to form</a>
+          </div>
+        </div>
+        <div className='headerBottom'>
+          <div className='headerBottom__text'>
+            <p>How  
+              <span className='headerBottom__text_green'>Over 410,000 Americans</span> Increased Their Property Value 
+              <span className='headerBottom__text_green'> In Under 6 Hours*</span> With This Simple Upgrade.
+            </p>
+            <p className='headerBottom__description'>Ideal for detached and semi-detached homes with sloped roofs.</p>
+          </div>
+          <div className='headerBottom__img'>
+            <img src={header} alt="house" className='headerBottom__img_big'/>
+            <img src={nbc} alt="nbc" className='headerBottom__img_sm'/>
           </div>
         </div>
       </div>
