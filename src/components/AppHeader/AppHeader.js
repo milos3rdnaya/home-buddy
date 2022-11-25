daa58dd126206639c1c5230c47ec70a5117cf30b
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2022-11-25 16:28:03 +0400

    feat(header): top header

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -2,13 +2,20 @@ import './appHeader.scss';
 
 export const AppHeader = () => {
 	return (
-		<header className="appHeader">
-      <a href='https://www.homebuddy.com/'>
-        <span>HomeBuddy</span>
-        <span>.</span> <br/>
-        <span>for homeowners</span>
-      </a>
-      <a>Link to form</a>
+		<header className="header">
+      <div className='container'>
+        <div className='headerTop'>
+          <div className='headerTop__logo'>
+            <a href='https://www.homebuddy.com/'>
+              <img src="https://www.homebuddy.com/static/shared1/homebuddy-logo.svg" alt="homebuddy logo"/>
+              <small className='headerTop__small'>for homeowners</small>
+            </a>
+          </div>
+          <div className='headerTop__anchor'>
+            <a href='#'>Link to form</a>
+          </div>
+        </div>
+      </div>
     </header>
 	);
 }
