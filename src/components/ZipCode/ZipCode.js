a64a2805079e74e821e7d71fc7252bacc01c5923
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2022-11-27 01:45:22 +0400

    feat(zip): desktop done

--- src/components/ZipCode/ZipCode.js
+++ src/components/ZipCode/ZipCode.js
@@ -4,15 +4,15 @@ export function ZipCode() {
   return (
     <section className="zipCode">
       <div className="container container_zipCode">
-        <h2 className="zipCode__header">See How Much Gutter Guards Cost In The XX, XXX Area</h2>
         <div className="zipCodeForm">
-          <label className="zipCodeForm__label">What is your ZIP Code?</label>
-          <div className="input-wrapper">
-            <input className="input" type="text" placeholder="  Enter ZIP Code"/>
-            <span>Label</span>
-          </div>
-          <small className="zipCodeForm__small">Free, no-obligation estimates.</small>
-          <button className="button">Get estimate</button>
+          <div className="zipCodeForm__wrapper">          
+            <label className="zipCodeForm__label">What is your ZIP Code?</label>
+            <div className="input-wrapper">
+              <input className="input" type="text" placeholder="  Enter ZIP Code"/>
+              <span>Enter ZIP Code</span>
+            </div>
+            <small className="zipCodeForm__small">Free, no-obligation estimates.</small>
+            <button className="button">Get estimate</button></div>
         </div>
       </div>
     </section>
